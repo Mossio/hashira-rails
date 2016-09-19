6a49c9ab29c97ed16f1cd17c41216225af20b498
Elliot Winkler <elliot.winkler@gmail.com>
2016-09-19 16:43:26 -0600

    Upgrade Rails to 5.0.0.1

--- lib/armadura/version.rb
+++ lib/armadura/version.rb
@@ -1,5 +1,5 @@
 module Armadura
-  RAILS_VERSION = "~> 5.0.0".freeze
+  RAILS_VERSION = "~> 5.0.0.1".freeze
   RUBY_VERSION = IO.
     read("#{File.dirname(__FILE__)}/../../.ruby-version").
     strip.
