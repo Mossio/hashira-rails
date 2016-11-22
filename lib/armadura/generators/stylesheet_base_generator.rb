f4cac1d30ccfe0e6d42f5570d871b6d55af33231
Elliot Winkler <elliot.winkler@gmail.com>
2016-11-21 22:43:21 -0600

    Upgrade Bourbon to 5.0.0.beta.7

--- lib/armadura/generators/stylesheet_base_generator.rb
+++ lib/armadura/generators/stylesheet_base_generator.rb
@@ -7,7 +7,7 @@ module Armadura
       File.dirname(__FILE__))
 
     def add_stylesheet_gems
-      gem "bourbon", "5.0.0.beta.6"
+      gem "bourbon", "5.0.0.beta.7"
       gem "neat", "~> 1.8.0"
       gem "refills", group: [:development, :test]
       Bundler.with_clean_env { run "bundle install" }
